17c7aa675b25dff5ab73d5b07b7138f29f3d0df0
pjury <pjury@protonmail.com>
2018-01-23 11:46:17 -0500

    completed login functionality

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,14 @@
 from app import app
+from flask import request
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Miguel'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -19,7 +20,7 @@ def index():
         }
     ]
 
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -28,14 +29,26 @@ def login():
         return redirect(url_for('index'))
 
     form = LoginForm()
+
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
+
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+
             return redirect(url_for('login'))
 
         login_user(user, remember=form.remember_me.data)
+
+        next_page = request.args.get('next')
+
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+
+            return redirect(next_page)
+
         return redirect(url_for('index'))
+
     return render_template('login.html', title='Sign In', form=form)
 
 
